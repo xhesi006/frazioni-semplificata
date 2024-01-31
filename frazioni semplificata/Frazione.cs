9584b1db69c450df3d7c6b20a277d0c14116ca56
nipol <nipol@LAPTOP-5ALMU75U>
2024-01-31 21:13:21 +0100

    Aggiungere i file di progetto.

---
+++ frazioni semplificata/Frazione.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace frazioni_semplificata
+{
+    internal class Frazione
+    {
+        public int N { get; set; }
+        public int D { get; set; }
+
+        public Frazione (int n, int d)
+        {
+            Semplifica(ref n, ref d);
+                N = n;
+                D = d;
+        }
+         private void Semplifica(ref int n, ref int d)
+        {
+            int mcd = CalcolaMcd(n, d);
+            n = n / mcd;
+            d = d / mcd;
+        }
+
+        private int CalcolaMcd(int a, int b)
+        {
+            int resto;
+            while (b != 0)
+            {
+                resto = a % b;
+                a = b;
+                b = resto;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return $"{N}/{D}";
+        }   
+
+    }
+}
---
+++ frazioni semplificata/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frazioni_semplificata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione f = new Frazione(9, 6);
+            Console.WriteLine(f);
+            Console.ReadLine();
+        }
+    }
+}
